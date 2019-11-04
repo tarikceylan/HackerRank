06b478b5165414a290e7de891a4b4cd87cc5c470
tarikceylan <tarikceylan@outlook.com>
2019-11-05 01:55:43 +0300

    Update Knagaroo.js

--- Knagaroo.js
+++ Knagaroo.js
@@ -3,19 +3,21 @@
 
 function kangaroo(x1, v1, x2, v2) {
     let notMet = true;
-    if(v2 > v1) {
+    if(v2 > v1 || v1 === v2) {
         return "NO";
     }
     else {
         while (notMet) {
             x1 += v1;
             x2 += v2;
-            if(x1 == x2) {
-                notMet = false;
-                return "YES";
+            if(x1 < x2 || x1 === x2) {
+                if(x1 === x2) {
+                    notMet = false;
+                    return "YES";
+                }
             }
             else {
-                notMet = true;
+                return "NO";
             }
         }
     }
